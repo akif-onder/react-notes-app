b586d0f7f16ef0493f817b71784256435a725474
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-17 00:29:04 +0300

    Note props...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
-import './App.css';
-import NotesList from './components/NotesList';
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [notes, setNotes] = useState(
+    [{
+    id: nanoid(),
+    text: "first note",
+    date: "16/07/2022",
+  },
+    {
+    id: nanoid(),
+    text: "second note",
+    date: "16/07/2022",
+  },
+    {
+    id: nanoid(),
+    text: "third note",
+    date: "16/07/2022",
+  },
+    {
+    id: nanoid(),
+    text: "4th note",
+    date: "16/07/2022",
+  },
+]
+  );
   return (
-    <div className="App">
-      <NotesList/>
+    <div className="container">
+      <NotesList notes={notes} />
     </div>
   );
 }
